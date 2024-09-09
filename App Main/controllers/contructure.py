b01fbcf141b521376b43cf934013a64729134f98
Gustavo Camacho <Gusttav28@GMAIL.COM>
2024-09-09 14:29:19 -0600

    the first method of the api its ready, that method is the one who add things to the db table

--- App Main/controllers/contructure.py
+++ App Main/controllers/contructure.py
@@ -1,46 +1,97 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
+from . import db_connection
 
 
 class app:
     def __init__(self, appFlask_name_):
         self.appFlask_name_ = appFlask_name_
+
+        #Constructure of flask
         self.appFlask_name_ = Flask(__name__)
-        self.route_home = "/"
-        self.route_tableView = "/tableView"
-        self.route_tableUpdate = "/tableUpdate"
-        self.route_tableDelete = "/tableDelete"
-        self.appFlask_name_.secret_key = "mysssecretkey"
+
+
+
+        #Creation of the route parameters
+        self.route_home = "/"    #home
+        self.route_login = "/login"  #login
+        self.route_userlogin = "/userlogin" #user login
+        self.route_tableView = "/tableView"  #this is the view of the general table
+        self.route_tableUpdate = "/tableUpdate"   #this is a page for make updates
+        self.route_tableDelete = "/tableDelete"  #and this is for delete (but it might will disappear because we might no need it)
+        self.appFlask_name_.secret_key = "mysssecretkey"  #secret key defination for POST http request  
+        
+        #this is the dictionary who have the user and the password of each user (this is just for test, than we are gonna change this for the data base) 
         self.users = {
             'gus': "1234",
             "adf": "adfas"
         }
 
-
+    # main method for run the application
     def app_main(self):
         self.appFlask_name_.run(port=8080, debug=True)
 
+    
+    #method for starts the routes
     def app_routes(self):
         @self.appFlask_name_.route(self.route_home)
+
+        #method for the home route
         def index():
             return render_template("home.html")
         
-        @self.appFlask_name_.route(self.route_tableView, methods=['GET'])
-        def table_view():
+
+        @self.appFlask_name_.route(self.route_userlogin)
+        def userlogin():
+            return render_template("userlogin.html")
+        
+
+        @self.appFlask_name_.route(self.route_login, methods=['GET'])
+        def handle_login():
             if request.method == 'GET':
                 username = request.args.get('username')
                 password = request.args.get('password')
                 if username in self.users and self.users[username] == password:
                     print(username, password)
-                    return "Welcome"
+                    return render_template("home.html") 
                 else:
                     return "invalid credentials"
             else:
-                return render_template("home.html")
+                return render_template("userlogin.html")
+
         
+        #method for the table view route
+        @self.appFlask_name_.route(self.route_tableView, methods=['POST', 'GET'])
+        def table_view():   
+            # return render_template("table_view.html")
+            data_list = []
+            if request.method == 'POST':
+                try:
+                    age = request.form['Age']
+                    name = request.form['Name'] 
+                    last_name = request.form['lastName']
+                    ocupation = request.form['Ocupation']
+                    print(age)
+                    print(name)
+                    print(last_name)
+                    print(ocupation)
+                    db = db_connection
+                    db.inserting_into_theDb(age, name, last_name, ocupation)
+                except:
+                    print("was some error while you was puting the data")
+                    return render_template("table_view.html")
+                for i in data_list:
+                    print(i)
+
+            return render_template("table_view.html")
+
+        #method for handle the login http request with [GET]
+        
+        #method for the table update route
         @self.appFlask_name_.route(self.route_tableUpdate)
         def table_update():
             return render_template("table_update.html")
         
+        #method for the table delete route (it could be delete)
         @self.appFlask_name_.route(self.route_tableDelete)
         def table_delete():
             return "This is the table delete page"
--- App Main/controllers/db_connection.py
+++ App Main/controllers/db_connection.py
@@ -17,30 +17,54 @@ def data_consulting():
     for i in data:
         print(i)
 
-def inserting_into_theDb():
+def inserting_into_theDb(age, first_name = str, last_name = str, ocupation = str):
+    age = age
+    first_name = first_name
+    last_name = last_name
+    ocupation = ocupation
     cur = cnn.cursor()
-    cur.execute('INSERT INTO user (id, age, first_name, last_name, ocupation, email, firstname, lastname) VALUES("1", "20", "Gustavo", "Camacho", "Software Engineer", "gusttav28@gmail.com", "Adolfo", "Vargas")')
+    cur.execute('INSERT INTO user (age, first_name, last_name, ocupation) VALUES(%s, %s, %s, %s)',(age, first_name, last_name, ocupation))
+    # cur.execute(f'INSERT INTO user (age, first_name, last_name, ocupation) VALUES({age}, {first_name}, {last_name}, {ocupation})')
     cnn.commit()
+    
 
-def updating_data():
+def updating_data(first_name = str, ocupation = str, id = int):
+    first_name = first_name
+    ocupation = ocupation
+    id = id
     cur = cnn.cursor()
-    cur.execute('UPDATE user SET ocupation = "Software Engineer"')
+    cur.execute('UPDATE user SET age = %s, first_name = %s, last_name = %s, ocupation = %s WHERE id = %s', (first_name, ocupation, id))
     cnn.commit()
-    cur.close()
+    
+
+def delete_data(id = int):
+    id = id
+    cur = cnn.cursor()
+    try:
+        cur.execute(f'DELETE FROM user WHERE id = {id}')
+        cnn.commit()
+        print(f"the user = {id} was delete successfully")
+    except:
+        print("It was some error, try again later")
+    cnn.close()
 
 
 
 # verifying the connection with the data base
-db_connection_test()
+# db_connection_test()
 
 # Inserting data into the table of the data base
-# inserting_into_theDb()
+# inserting_into_theDb(20, "Daniela", "Vindas", "Psichology Doctor - Neurological")
 
+# updating data from the data base
+# updating_data("Juan", "Mechanics Engineer", 2)
 
-updating_data()
+# delete data from the data base
+# delete_data(14)
 
 # collecting general data from the data base
-data_consulting()
+# data_consulting()
+
 
 
 
